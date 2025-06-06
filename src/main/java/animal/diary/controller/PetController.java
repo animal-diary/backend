e63fe57991dfc6ef7255e4386f2af28e43a6fa04
이규민 <kyumin991105@duksung.ac.kr>
2025-06-06 22:33:04 +0900

    ✨ [FEAT] 여러 동물들을 가져올 수 있어 url 을 pets 로 변경

--- src/main/java/animal/diary/controller/PetController.java
+++ src/main/java/animal/diary/controller/PetController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/pet")
+@RequestMapping("/pets")
 @RequiredArgsConstructor
 public class PetController {
     private final PetService petService;
